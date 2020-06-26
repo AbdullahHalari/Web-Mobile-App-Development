087173ca614b651ed98f364810207f8af65d1539
AbdullahHalari <droidabdullah@gmail.com>
2020-06-27 01:56:39 +0500

    JScalculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -12,10 +12,3 @@ function getResult(){
     var result = document.getElementById("result");
     result.value = eval(result.value)
 }
-function clearone() {
-    var getnumber = document.getElementById("input");
-    var str = input.value;
-    var num = str.length;
-    var str1 = str.slice(0, num - 1);
-    input.value = str1;
-}
